7234fd42b58610b5a197dc5f92d22f15f4270cdc
luthfirrohman <luthfiroh200@gmail.com>
2021-07-08 16:05:57 +0700

    Added Bottom Navigation View

--- app/src/main/java/com/smart/smartbalibackpaker/MainActivity.kt
+++ app/src/main/java/com/smart/smartbalibackpaker/MainActivity.kt
@@ -2,10 +2,24 @@ package com.smart.smartbalibackpaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.smart.smartbalibackpaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
+        navView.itemIconTintList = null
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
+        navView.drawableState
     }
 }
