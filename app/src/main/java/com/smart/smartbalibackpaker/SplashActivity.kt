65d06cb3fca0254017c48bdf13d310da4cf34881
mohnaufalp2000 <mohammadnp2000@gmail.com>
2021-07-10 09:01:39 +0700

    add splash screen

---
+++ app/src/main/java/com/smart/smartbalibackpaker/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.smart.smartbalibackpaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.smart.smartbalibackpaker.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private val binding by lazy{ActivitySplashBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        Handler(mainLooper).postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finishAffinity()
+        }, 2000)
+    }
+}
