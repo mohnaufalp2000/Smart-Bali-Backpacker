ae3834ab37af2956631c18c4c43359a87d788dee
mohnaufalp2000 <mohammadnp2000@gmail.com>
2021-07-11 16:29:45 +0700

    add login and sign up page

---
+++ app/src/main/java/com/smart/smartbalibackpaker/auth/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.smart.smartbalibackpaker.auth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smart.smartbalibackpaker.MainActivity
+import com.smart.smartbalibackpaker.R
+import com.smart.smartbalibackpaker.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy {ActivityLoginBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/smart/smartbalibackpaker/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.smart.smartbalibackpaker.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smart.smartbalibackpaker.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
