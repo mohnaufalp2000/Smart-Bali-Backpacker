bc0a4a456847a76f6916af00a29a642c474f8a75
mohnaufalp2000 <mohammadnp2000@gmail.com>
2021-07-13 05:20:01 +0700

    fix tab layout in dashboard fragment

--- app/src/main/java/com/smart/smartbalibackpaker/MainActivity.kt
+++ app/src/main/java/com/smart/smartbalibackpaker/MainActivity.kt
@@ -2,23 +2,27 @@ package com.smart.smartbalibackpaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.smart.smartbalibackpaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(activityMainBinding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navView: BottomNavigationView = findViewById(R.id.bottom_nav_view)
         navView.itemIconTintList = null
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+//        val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
         navView.drawableState
     }
---
+++ app/src/main/java/com/smart/smartbalibackpaker/core/preferences/PreferencesSettings.kt
@@ -0,0 +1,66 @@
+package com.smart.smartbalibackpaker.core.preferences
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
+import com.smart.smartbalibackpaker.R
+
+
+class PreferencesSettings : PreferenceFragmentCompat() {
+
+    private lateinit var editProfilePreferences: Preference
+    private lateinit var notificationsPreferences: SwitchPreference
+    private lateinit var languagesPreferences: Preference
+    private lateinit var editProfile: String
+    private lateinit var notifications: String
+    private lateinit var languages: String
+
+    override fun setDivider(divider: Drawable?) {
+        super.setDivider(divider)
+        resources.getDrawable(R.drawable.divider_settings)
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, "settings_preferences")
+        setupEditProfile()
+        setupNotifications()
+        setupLanguages()
+    }
+
+    private fun setupEditProfile() {
+        editProfile = resources.getString(R.string.key_edit_profile)
+        editProfilePreferences = findPreference<Preference>(editProfile) as Preference
+        editProfilePreferences.setOnPreferenceClickListener {
+            Toast.makeText(context, "EDIT PROFILE", Toast.LENGTH_LONG).show()
+            true
+        }
+    }
+
+    private fun setupNotifications() {
+        notifications = resources.getString(R.string.key_notifications)
+        notificationsPreferences = findPreference<SwitchPreference>(notifications) as SwitchPreference
+
+        notificationsPreferences.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener{ _,_ ->
+                notificationsPreferences.isChecked = !notificationsPreferences.isChecked
+
+                true
+            }
+    }
+
+    private fun setupLanguages(){
+        languages = resources.getString(R.string.key_edit_languages)
+        languagesPreferences = findPreference<Preference>(languages) as Preference
+        languagesPreferences.setOnPreferenceClickListener {
+            val intent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(intent)
+            true
+        }
+    }
+}
