c3164993de60eb16ac49f9ab7ce456147ae20efe
luthfirrohman <luthfiroh200@gmail.com>
2021-09-17 16:05:54 +0700

    Added new First Regist Form

--- app/src/main/java/com/smart/smartbalibackpaker/registration/RegistFirstFormActivity.kt
+++ app/src/main/java/com/smart/smartbalibackpaker/registration/RegistFirstFormActivity.kt
@@ -1,12 +1,18 @@
 package com.smart.smartbalibackpaker.registration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.core.widget.doOnTextChanged
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.smart.smartbalibackpaker.R
+import com.smart.smartbalibackpaker.dashboard.DashboardFragment
 import com.smart.smartbalibackpaker.databinding.ActivityRegistFirstFormBinding
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class RegistFirstFormActivity : AppCompatActivity() {
 
@@ -19,6 +25,30 @@ class RegistFirstFormActivity : AppCompatActivity() {
 
         binding.btnRegistDatepicker.setOnClickListener {
             showDataRangePicker()
+        }
+
+        val items = listOf("Pelabuhan Gilimanuk", "Pelabuhan Benoa", "Bandara Internasional Ngurah Rai")
+        val adapter = ArrayAdapter(this, R.layout.list_item, items)
+        (binding.listArrivalPlace as? AutoCompleteTextView)?.setAdapter(adapter)
+
+        binding.apply {
+            btnNextRegister.setOnClickListener {
+                when {
+                    etRegistFillDate1.text.toString().trim().isEmpty() -> {
+                        etRegistFillDate1.error = "Tanggal kedatangan harus diisi"
+                        etRegistFillDate2.error = "Tanggal kepulangan harus diisi"
+                    }
+                    edtRegistName.text.toString().trim().isEmpty() -> {
+                        edtRegistName.error = "Nama backpacker harus diisi"
+                    }
+                    edtRegistPhone.text.toString().trim().isEmpty() -> {
+                        edtRegistPhone.error = "No. Telp backpacker harus diisi"
+                    }
+                    listArrivalPlace.text.toString().trim().isEmpty() -> {
+                        listArrivalPlace.error = "Tempat kedatangan harus dipilih"
+                    } else -> startActivity(Intent(this@RegistFirstFormActivity, DashboardFragment::class.java))
+                }
+            }
         }
     }
 
@@ -41,12 +71,15 @@ class RegistFirstFormActivity : AppCompatActivity() {
             val startDate = dateSelected.first
             val endDate = dateSelected.second
 
-//            if (startDate != null && endDate != null) {
-//                binding.etRegistDate1.editText?.doOnTextChanged { date,_,_,_ ->  it = startDate}
-////                =
-////                    "Reserved\nStartDate: ${convertLongToTime(startDate)}\n" +
-////                            "EndDate: ${convertLongToTime(endDate)}"
-//            }
+            val vacationDuration = TimeUnit.DAYS.convert(endDate-startDate, TimeUnit.MILLISECONDS)
+
+            if (startDate != null && endDate != null) {
+                binding.etRegistFillDate1.setText("${convertLongToTime(startDate)}")
+                binding.etRegistFillDate2.setText("${convertLongToTime(endDate)}")
+                binding.btnRegistDatepicker.text = "$vacationDuration Day(s)"
+            }
+
+
         }
 
     }
